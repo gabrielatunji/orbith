70853b8963417cca3f5ebf240fb305457f255e59
unknown <ola.gabriel19@gmail.com>
2025-10-03 20:41:13 +0100

    move repos

--- src/routes/inquiryroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const inquiryController = require('../controllers/inquirycontroller');
-
-router.post('/', inquiryController.createInquiry);
-router.get('/domain/:domainName', inquiryController.getByDomain);
-
-module.exports = router;
-
--- src/routes/watchroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const watchController = require('../controllers/watchController');
-
-router.post('/', watchController.createWatch);
-router.get('/', watchController.listWatches);
-router.delete('/:id', watchController.deleteWatch);
-
-module.exports = router;
