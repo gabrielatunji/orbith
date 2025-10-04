13d1196be91db0a120938acce0a487fcef53b7c5
Moyo Adeniji <126980774+couch-p0tato@users.noreply.github.com>
2025-10-04 05:02:54 +0100

    Update Link to Web App

--- src/components/TradeSmarter.jsx
+++ src/components/TradeSmarter.jsx
@@ -2,6 +2,11 @@ import image from "../assets/image.png";
 import mask from "../assets/mask.png";
 
 export default function TradeSmarter() {
+
+  const handleClick = () => {
+    window.open("https://orbith-b8tw.onrender.com/", "_blank");
+  };
+  
   return (
     <section className="py-20 px-6 bg-gradient-to-b from-black-900 to-gray-900">
       <div className="max-w-7xl mx-auto text-center">
@@ -13,7 +18,7 @@ export default function TradeSmarter() {
           data-driven scores, chat with owners, and trade <br /> securely
           on-chain - all in one place.
         </p>
-        <button className="self-start bg-blue-700 text-white text-sm mt-4 font-medium py-3 px-4 rounded-xl shadow-md hover:bg-blue-800 hover:scale-105 transition duration-200">
+        <button onClick={handleClick} className="cursor-pointer self-start bg-blue-700 text-white text-sm mt-4 font-medium py-3 px-4 rounded-xl shadow-md hover:bg-blue-800 hover:scale-105 transition duration-200">
           Let's get started
         </button>
         <div className="mt-16 mx-auto w-full max-w-4xl">
